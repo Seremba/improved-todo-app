932c905ddc6fcb4f2c142b18086531b3f129b63d
Seremba <pserembae@gmail.com>
2021-01-24 12:25:49 +0300

    add functionality with javascript for adding lists, remove lists, storing lists to local storage

--- script.js
+++ script.js
@@ -1,14 +1,51 @@
 const listsContainer = document.querySelector('[data-lists]')
+const newListForm = document.querySelector('[data-new-list-form]')
+const newListInput = document.querySelector('[data-new-list-input]')
 
-let lists = ['name', 'todo']
+const LOCAL_STORAGE_LIST_KEY = "task.lists"
+const LOCAL_STORAGE_SELECTED_LIST_KEY = 'task.selectedListId'
+let lists = JSON.parse(localStorage.getItem(LOCAL_STORAGE_LIST_KEY)) || [] 
+let selectedListId = localStorage.getItem(LOCAL_STORAGE_SELECTED_LIST_KEY)
 
+listsContainer.addEventListener('click', e => {
+    if(e.target.tagName.toLowerCase() === 'li') {
+        selectedListId = e.target.dataset.listId
+        saveAndRender()
+    }
+})
+newListForm.addEventListener('submit', e => {
+    e.preventDefault()
+    const listName = newListInput.value 
+    if(listName == null || listName === '') return 
+    const list = createList(listName)
+    newListInput.value = null 
+    lists.push(list) 
+    saveAndRender()
+})
+
+function createList(name) {
+    return {id: Date.now().toString(), name: name, tasks: []}
+}
+
+function saveAndRender(){
+    save()
+    render()
+}
+
+function save() {
+    localStorage.setItem(LOCAL_STORAGE_LIST_KEY, JSON.stringify(lists))
+    localStorage.setItem(LOCAL_STORAGE_SELECTED_LIST_KEY ,selectedListId) 
+}
 function render() {
     clearElement(listsContainer)
-    // <li class="list-name">Work</li>
   lists.forEach(list => {
       const listElement = document.createElement('li')
+      listElement.dataset.listId = list.id 
       listElement.classList.add("list-name")
-      listElement.innerHTML = list
+      listElement.innerHTML = list.name
+      if(list.id === selectedListId){
+          listElement.classList.add('active-list')
+      }
       listsContainer.appendChild(listElement)
   })
 }
